9f3c6c61bb7559af8baac53ccf1da78d1f7cd2dc
rajin4884 <rajin4884@gmail.com>
2017-10-16 20:48:47 +0900

    djangonew complete

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '403fil5w-3xthglf3ibvu4n8y5f+my&uce68d3q#m25-1pk91@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
